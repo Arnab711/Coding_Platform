6ea38136ad4c203a0f0410c83472690168da2ca0
Arnab711 <21052817@kiit.ac.in>
2024-06-27 21:33:46 +0530

    Created roadmap home page . Also,added a navbar header for reference.

--- Coding_platform/coding_platform/coding_platform/settings.py
+++ Coding_platform/coding_platform/coding_platform/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+    'home',
+    'roadmap'
 ]
 
 MIDDLEWARE = [
@@ -115,10 +116,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
-
+STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR /'static',
+]
