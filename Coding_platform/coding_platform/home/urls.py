36c72a2c43ca48e66ceeec8b9d859b8a39ac16a2
Arnab711 <21052817@kiit.ac.in>
2024-06-30 02:45:27 +0530

    Created and tested register and login page

--- Coding_platform/coding_platform/home/urls.py
+++ Coding_platform/coding_platform/home/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('roadmap/',views.roadmap_page, name='roadmap_view'),
     path('aboutus/',views.aboutus_page, name='aboutus_view'),
     path('contact/',views.contact_page, name='contact_view'),
-	path('register/', views.register_page, name='register_page'),
-	
+	path('login/', views.login_page, name='login_page'),
+    path('register/', views.register_page, name='register_page'),
+    path('restricted/', views.restricted_view, name='restricted'),
+    path('logout/',views.logout_page, name='logout'),
 ]
--- Coding_platform/coding_platform/home/views.py
+++ Coding_platform/coding_platform/home/views.py
@@ -1,25 +1,63 @@
-from django.shortcuts import render
-
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import login, authenticate,logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.contrib import messages
+from .forms import CreateUserForm
 # Create your views here.
+
+
+@login_required(login_url='login_page')
 def index(request):
     return render(request,'index.html')
+
+@login_required(login_url='login_page')
 def roadmap_page(request):
     return render(request,'roadmap_home.html')
+
+@login_required(login_url='login_page')
 def aboutus_page(request):
     return render(request,'aboutus.html')
+
+@login_required(login_url='login_page')
 def contact_page(request):
     return render(request,'contact.html')
 
 def register_page(request):
-    """
-    Returns the registration page.
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = CreateUserForm()
+    return render(request, 'register.html', {'form': form})
 
-    Returns
-    -------
-    HTTPResponse
-        The registration page.
-    """
-    return render(request, 'register.html')
 
+@login_required(login_url='login_page')
+def restricted_view(request):
+    return HttpResponse("This is a restricted view. You are logged in!")
+
+
+def login_page(request):
+    if request.method == 'POST':
+        un=request.POST['username']
+        pd=request.POST['password']
+        user=authenticate(request,username=un,password=pd)
+        if user is not None:
+            login(request,user)
+            return redirect('index')
+        else:
+            messages.error(request,'Username or Password is Incorrect')
+    return render(request, 'login.html')
+
+def logout_page(request):
+    logout(request)
+    return redirect('login_page')
 
