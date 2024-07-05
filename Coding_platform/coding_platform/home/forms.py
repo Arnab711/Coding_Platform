426980b5710cfb0bb284ef201ca8b114e4fc41b2
Arnab711 <21052817@kiit.ac.in>
2024-07-05 19:46:44 +0530

    Changed background color of index.html and removed help text from register page

--- Coding_platform/coding_platform/home/forms.py
+++ Coding_platform/coding_platform/home/forms.py
@@ -7,3 +7,9 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(CreateUserForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'email', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
